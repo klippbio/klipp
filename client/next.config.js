56b1fe731382e97cb2142589f4db9ef2b7f37c6d
{KevalJagani} <kevaljagani0119@gmail.com>
2023-08-18 21:10:18 -0400

    onboarding backend

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,14 @@
-module.exports = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/:slug*',
-                destination: 'http://localhost:4000/api/:slug*'
-            },
-        ]
-    },
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:4000/:path*",
+      },
+    ];
+  },
+};
+module.exports = nextConfig;
--- server/routes.ts
+++ server/routes.ts
@@ -2,16 +2,12 @@ import express from "express";
 import { Request, Response } from "express";
 import { ClerkExpressWithAuth } from "@clerk/clerk-sdk-node";
 import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
-import { userRoute } from "./routes/UserRoute";
+import { userController } from "./controllers/userController";
 
 export const routes = express.Router();
 
-routes.use("/user", userRoute);
+routes.use("/user", userController);
 
-routes.get("/api", async (req: Request, res: Response) => {
-  res.json({ users: ["user1", "user2", "sesd"] });
-});
-
-routes.post("/api/validate", ClerkExpressRequireAuth(), async (req, res) => {
-  res.send("validate1");
-});
+// routes.post("/api/validate", ClerkExpressRequireAuth(), async (req, res) => {
+//   res.send("validate1");
+// });
