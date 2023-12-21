a54061c46a46d40b9bf8720980508b1ffdd2ac68
{KevalJagani} <kevaljagani0119@gmail.com>
2023-12-21 02:02:45 -0500

    missing files

--- server/controllers/paymentController.ts
+++ server/controllers/paymentController.ts
@@ -1,65 +1,79 @@
 import express from "express";
 import { Request, Response } from "express";
-
-export const paymentController = express.Router();
 import {
   createAccount,
   getAccountDetails,
+  handleDeleteAccountFromDB,
+  handleUpdateAccount,
 } from "../services/payment/paymentService";
 import CustomError from "../utils/CustomError";
+import { env } from "process";
 
-paymentController.get("/test", async (req: Request, res: Response) => {
-  return res.status(200).json("test");
-});
+export const paymentController = express.Router();
 
-paymentController.post("/create", async (req: Request, res: Response) => {
-  const stripe = require("stripe")(
-    "sk_test_51ODeaHKiQFEEPGdjBvEPJUUSO6mstnNQFzy03aRDJxTVfgXriuthpcZ6j2ppMJjR1vVXtQqOUBxekyCKUl4cYOAR003xGDEGAI"
-  );
+const STRIPE_TEST_KEY = env.STRIPE_TEST_KEY;
+const stripe = require("stripe")(STRIPE_TEST_KEY);
 
-  //TODO: Config params properly
+paymentController.post("/connect", async (req: Request, res: Response) => {
+  //TODO
+  //Config params properly
   //Config Merchant codes
   //Config service aggreement based on locations. IF Us full else receipeint
   //Check CSRF token issue
   //Use publishable key and secret key intead of test keyt
-
-  const account = await stripe.accounts.create({
-    country: "US",
-    type: "express",
-    capabilities: {
-      card_payments: {
-        requested: true,
+  //handle unfinished onboarding
+  try {
+    //creating stripe account
+    const account = await stripe.accounts.create({
+      country: req.body.country,
+      type: "express",
+      capabilities: {
+        card_payments: {
+          requested: true,
+        },
+        transfers: {
+          requested: true,
+        },
       },
-      transfers: {
-        requested: true,
+      business_profile: {
+        url: "https://klipp.io",
       },
-    },
-    business_type: "individual",
-    business_profile: {
-      url: "https://klipp.io",
-    },
-  });
+    });
 
-  await createAccount({ accountId: account.id });
+    //saving details in db
+    await createAccount({ accountId: account.id });
 
-  const accountLink = await stripe.accountLinks.create({
-    account: account.id,
-    refresh_url: "https://klipp.io/reauth",
-    return_url: "https://klipp.io/return",
-    type: "account_onboarding",
-  });
+    //generating account link
+    const accountLink = await stripe.accountLinks.create({
+      account: account.id,
+      refresh_url: "https://klipp.io/reauth",
+      return_url: "http://localhost:3000/payments",
+      type: "account_onboarding",
+    });
 
-  //stoire account id in db
-
-  res.status(200).json(accountLink);
+    res.status(200).json(accountLink);
+  } catch (error) {
+    if (error instanceof CustomError)
+      res.status(error.statusCode).json({ error: error.message });
+    else res.status(500).json({ error: error });
+  }
 });
 
-paymentController.get(
-  "/accountDetails",
+paymentController.post(
+  "/pendingAccount",
   async (req: Request, res: Response) => {
     try {
-      const accountDetails = await getAccountDetails();
-      res.status(201).json(accountDetails);
+      //creating stripe account
+      const accountId = req.body.accountId;
+      //generating account link
+      const accountLink = await stripe.accountLinks.create({
+        account: accountId,
+        refresh_url: "https://klipp.io/reauth",
+        return_url: "http://localhost:3000/payments",
+        type: "account_onboarding",
+      });
+
+      res.status(200).json(accountLink);
     } catch (error) {
       if (error instanceof CustomError)
         res.status(error.statusCode).json({ error: error.message });
@@ -67,3 +81,150 @@ paymentController.get(
     }
   }
 );
+
+paymentController.post(
+  "/webhooks/stripe",
+  async (req: Request, res: Response) => {
+    try {
+      if (req.body.type === "account.updated") {
+        handleUpdateAccount(req.body.data.object);
+      }
+      res.status(200).json("ok");
+    } catch (error) {
+      if (error instanceof CustomError)
+        res.status(error.statusCode).json({ error: error.message });
+      else res.status(500).json({ error: error });
+    }
+  }
+);
+
+paymentController.post("/disconnect", async (req: Request, res: Response) => {
+  try {
+    // Deleting the Stripe account
+    const deleted = await stripe.accounts.del(req.body.accountId);
+
+    if (deleted.deleted) {
+      await handleDeleteAccountFromDB(req.body);
+
+      res.status(200).json({ message: "Account successfully disconnected" });
+    } else {
+      res.status(500).json({ error: "Failed to disconnect the account" });
+    }
+  } catch (error) {
+    if (error instanceof CustomError)
+      res.status(error.statusCode).json({ error: error.message });
+    else res.status(500).json({ error: error });
+  }
+});
+
+paymentController.post(
+  "/create-checkout-session",
+  async (req: Request, res: Response) => {
+    const stripe = require("stripe")(
+      "sk_test_51ODeaHKiQFEEPGdjBvEPJUUSO6mstnNQFzy03aRDJxTVfgXriuthpcZ6j2ppMJjR1vVXtQqOUBxekyCKUl4cYOAR003xGDEGAI"
+    );
+
+    const session = await stripe.checkout.sessions.create({
+      mode: "payment",
+      line_items: [
+        {
+          quantity: 1,
+          price_data: {
+            currency: "cad",
+            product_data: {
+              name: "Stubborn Attachments",
+              images: ["https://i.imgur.com/EHyR2nP.png"],
+            },
+            unit_amount: 2000,
+          },
+        },
+      ],
+      payment_intent_data: {
+        application_fee_amount: 123,
+        transfer_data: {
+          destination: "acct_1OLvmx4EIvJZrDDi",
+        },
+        on_behalf_of: "acct_1OLvmx4EIvJZrDDi",
+      },
+      success_url: "https://example.com/success",
+      cancel_url: "https://example.com/cancel",
+    });
+
+    res.status(200).json(session.url);
+  }
+);
+
+//TODO: Change this whole controller.
+paymentController.get(
+  "/stripeAccountDetails",
+  async (req: Request, res: Response) => {
+    try {
+      const accountDetails = await getAccountDetails();
+      const latestAccountDetail = accountDetails?.reduce((max, account) =>
+        account.id > max.id ? account : max
+      );
+      res.status(201).json(latestAccountDetail);
+    } catch (error) {
+      if (error instanceof CustomError)
+        res.status(error.statusCode).json({ error: error.message });
+      else res.status(500).json({ error: error });
+    }
+  }
+);
+
+paymentController.get(
+  "/stripeaccountbalance",
+  async (req: Request, res: Response) => {
+    const accId = req.query.stripeAccountId;
+
+    try {
+      const balance = await stripe.balance.retrieve({
+        stripeAccount: accId,
+      });
+
+      res.status(200).json(balance);
+    } catch (error) {
+      console.log(error);
+      if (error instanceof CustomError)
+        res.status(error.statusCode).json({ error: error.message });
+      else res.status(500).json({ error: error });
+    }
+  }
+);
+
+paymentController.post("/payout", async (req: Request, res: Response) => {
+  try {
+    console.log("req.body ", req.body);
+    const balance = await stripe.balance.retrieve({
+      stripeAccount: req.body.accountId,
+    });
+    const { amount, currency } = balance.available[0];
+    console.log("amount ", amount);
+    // Create a payout
+    const payout = await stripe.payouts.create(
+      {
+        amount: amount,
+        currency: currency,
+        statement_descriptor: "aasas",
+      },
+      { stripeAccount: req.body.accountId }
+    );
+    res.status(200).json("success");
+  } catch (error) {
+    // console.log(error);
+    if (error instanceof CustomError)
+      res.status(error.statusCode).json({ error: error.message });
+    else res.status(500).json({ error: error });
+  }
+});
+
+paymentController.post("/dashboard", async (req: Request, res: Response) => {
+  try {
+    const loginLink = await stripe.accounts.createLoginLink(req.body.accountId);
+    res.status(200).json(loginLink);
+  } catch (error) {
+    if (error instanceof CustomError)
+      res.status(error.statusCode).json({ error: error.message });
+    else res.status(500).json({ error: error });
+  }
+});
--- server/services/payment/paymentService.ts
+++ server/services/payment/paymentService.ts
@@ -1,14 +1,9 @@
 import { z } from "zod";
 import { db } from "../../utils/db.server";
-import type { User } from "@prisma/client";
 
 export const ZAccountSchema = z.object({
   accountId: z.string(),
 });
-
-export const test = async (): Promise<User[]> => {
-  return db.user.findMany({});
-};
 
 export const createAccount = async (input: z.infer<typeof ZAccountSchema>) => {
   const storeId = "e2444f66-3887-41de-b03f-aa807828879d";
@@ -25,6 +20,32 @@ export const createAccount = async (input: z.infer<typeof ZAccountSchema>) => {
   return account;
 };
 
+export const handleUpdateAccount = async (input: any) => {
+  //get account from db based on id from event
+  //update charges enabled, transfers enabled, payouts enabled and onboarding complete details
+
+  const account = await db.payment.update({
+    where: {
+      accountId: input.id,
+    },
+    data: {
+      onboardingComplete: input.details_submitted,
+    },
+  });
+  return account;
+};
+
+export const handleDeleteAccountFromDB = async (input: any) => {
+  //get account from db based on id from event
+  //update charges enabled, transfers enabled, payouts enabled and onboarding complete details
+  const account = await db.payment.delete({
+    where: {
+      accountId: input.accountId,
+    },
+  });
+  return account;
+};
+
 export const getAccountDetails = async () => {
   const storeId = "e2444f66-3887-41de-b03f-aa807828879d";
 
@@ -35,5 +56,19 @@ export const getAccountDetails = async () => {
     },
   });
 
+  if (accountDetails.length === 0) {
+    return null;
+  }
   return accountDetails;
+};
+
+export const getAccountById = async (id: any) => {
+  //this should be unique change it once testing is done
+  const account = await db.payment.findMany({
+    where: {
+      accountId: id,
+    },
+  });
+
+  return account;
 };
