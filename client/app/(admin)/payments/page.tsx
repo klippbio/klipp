a56b5192ea657e0d698d7987288ca9be7c4bde31
{KevalJagani} <kevaljagani0119@gmail.com>
2023-12-21 02:01:35 -0500

    Stripe payment details and connect

---
+++ client/app/(admin)/payments/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import { PaymentMethods } from "./components/PaymentMethods";
+import PaymentDetails from "./components/PaymentDetails";
+
+function Payments() {
+  return (
+    <div className="w-full">
+      <div className="flex md:flex-row flex-col gap-4 w-full">
+        <div className="md:w-2/3 w-full">
+          <PaymentDetails />
+        </div>
+        <div className="md:w-1/3 w-full">
+          <PaymentMethods />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Payments;
