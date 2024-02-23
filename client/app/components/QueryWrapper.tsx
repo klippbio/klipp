338ec98a6d5bca1d68b51a039f7df4213fe1d106
Meet Shukla <meet22599@gmail.com>
2024-02-22 23:59:04 -0500

    removing react query

--- client/app/components/QueryWrapper.tsx
+++ client/app/components/QueryWrapper.tsx
@@ -2,7 +2,6 @@
 
 import React, { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,10 +16,7 @@ interface Props {
 }
 
 const QueryWrapper = ({ children }: Props) => (
-  <QueryClientProvider client={queryClient}>
-    <Toaster />
-    {children}
-  </QueryClientProvider>
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 );
 
 export default QueryWrapper;
