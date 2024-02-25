df159bda8a7e607fedf0261174caf9ab021a0979
{KevalJagani} <kevaljagani0119@gmail.com>
2024-02-25 01:26:33 -0500

    webhook

--- server/controllers/webhookController.ts
+++ server/controllers/webhookController.ts
@@ -8,15 +8,19 @@ export const webhookController = express.Router();
 
 webhookController.post("/", async (req: Request, res: Response) => {
   try {
+    console.log("entry to webhook1", req.body);
     if (req.body.type === "account.updated") {
       handleUpdateAccount(req.body.data.object);
     }
 
     if (req.body.type === "checkout.session.completed") {
+      console.log("entry to webhook2", req.body);
       const session = req.body;
 
       const saleId = session.data.object.metadata.saleId;
+      console.log("sale id in webhook", saleId);
       await updateSaleStatus(saleId, "COMPLETED");
+      console.log("Edit webhook");
     }
   } catch (error) {
     console.log(error);
