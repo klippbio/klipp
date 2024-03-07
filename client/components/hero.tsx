c897d3fc0eec75532ba90c6ddd3c44b8cab7bdb5
{KevalJagani} <kevaljagani0119@gmail.com>
2024-03-07 01:50:47 -0500

    Update Hero component styling

--- client/components/hero.tsx
+++ client/components/hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <>
       <Container className="flex flex-wrap ">
         <div className="flex items-center w-full lg:w-1/2">
-          <div className="max-w-2xl mb-8">
+          <div className="max-w-2xl md:mb-44 mb-8">
             <h1 className="text-4xl mb-2 font-medium leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
               Turn{" "}
               <span className="font-extrabold bg-gradient-to-r from-purple-600 via-blue-500 to-purple-400 inline-block text-transparent bg-clip-text">
@@ -19,13 +19,13 @@ const Hero = () => {
               with your Digital Store
               <br />
             </h1>
-            <p className="py-10 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
+            <p className="py-8 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
               Sell digital products, schedule meetings, offer coaching, and
               consolidate links in your link-in-bio store. Your one-stop
               monetization platform.
             </p>
 
-            <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
+            <div className="flex mt-3 flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <a
                 href={process.env.NEXT_PUBLIC_FRONTEND_URL + "/sign-up"}
                 target="_blank"
