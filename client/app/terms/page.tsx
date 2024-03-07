3f8d25560afd8b7ac1bd191fb8e1e9b0e5b9a153
Meet Shukla <meet22599@gmail.com>
2024-03-06 23:18:54 -0500

    Terms and conditions introduced!

---
+++ client/app/terms/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+import Terms from "@/components/terms";
+
+const TermsofService = () => {
+  return (
+    <div className="bg-white">
+      <Navbar />
+
+      <Terms />
+      <Footer />
+    </div>
+  );
+};
+
+export default TermsofService;
---
+++ client/components/terms.tsx
@@ -0,0 +1,147 @@
+"use client";
+import React from "react";
+
+function TermsOfService() {
+  return (
+    <div id="klipp-terms-service" className="max-w-4xl mx-auto p-5 space-y-4">
+      <h1 className="text-2xl font-bold text-center mb-6">
+        KLIPP TERMS OF SERVICE
+      </h1>
+      <p className="text-justify mb-4">
+        These Terms of Service ("Terms") govern your use of Klipp services
+        ("Service" or "Klipp") provided by Klipp . They constitute a binding
+        agreement between you and the klipp. The terms outline your rights and
+        obligations regarding your use of Klipp, including any content (videos,
+        audio, images, text, or other materials—collectively referred to as
+        "Content") you encounter or interact with. The Service may include
+        Content not owned by Klipp, known as "User Content," which remains the
+        sole responsibility of its provider.
+      </p>
+
+      <p className="text-justify mb-4">
+        By engaging with Klipp, you consent to these Terms, applicable laws and
+        regulations, and affirm compliance with local laws. Non-agreement with
+        these Terms bars you from Service access or use. Content within Klipp is
+        safeguarded by copyright and trademark laws.
+      </p>
+
+      <p className="text-justify mb-4">
+        Klipp reserves the right to modify these Terms without notice. Continual
+        Klipp usage post-modification implies acceptance of the revised Terms.
+        Periodically review these Terms; if any revision is unacceptable,
+        discontinue use and contact Klipp (klipp.bio@gmail.com) to terminate
+        your account, subject to the new Terms otherwise.
+      </p>
+
+      <h2 className="text-xl font-semibold mb-4">Age Limitations</h2>
+      <p className="text-justify mb-4">
+        Confirming acceptance of these Terms or using the Services asserts you
+        are at least 18 years old. The Service is off-limits if you are
+        prohibited under the laws of your residence or access point.
+      </p>
+
+      <h2 className="text-xl font-semibold mb-4">Service Overview</h2>
+      <p className="text-justify mb-4">
+        Klipp facilitates a digital platform for Users to share, access, and
+        engage with content. The Service acts as an intermediary and does not
+        own, control, or endorse any User Content. Users, whether providing or
+        consuming content, bear full responsibility for their interactions
+        within Klipp.
+      </p>
+
+      <p className="text-justify mb-4">
+        You acknowledge that Klipp can modify, replace, or discontinue any
+        Service aspect without liability at its discretion.
+      </p>
+
+      <h2 className="text-xl font-semibold mb-4">Content Responsibility</h2>
+      <p className="text-justify mb-4">
+        You retain rights to your uploaded, submitted, or posted content. You
+        are accountable for this content's legality and adherence to these
+        Terms. Klipp disclaims endorsement or liability for Service content,
+        emphasizing user discretion in exposure to potentially harmful or
+        offensive User Content.
+      </p>
+
+      <p className="text-justify mb-4">
+        Klipp reserves the right to manage content to comply with legal
+        obligations, maintain service integrity, or enforce these Terms.
+      </p>
+
+      <h2 className="text-xl font-semibold mb-4">Prohibited Content</h2>
+      <ul className="list-disc list-inside mb-4">
+        <li>Violates any law or infringes on rights of others.</li>
+        <li>
+          Is harmful, fraudulent, deceptive, threatening, harassing, defamatory,
+          obscene, or otherwise objectionable.
+        </li>
+        <li>
+          Promotes discrimination, bigotry, racism, hatred, harassment, or harm
+          against any individual or group.
+        </li>
+        <li>Is sexually explicit or promotes pornography.</li>
+        <li>
+          Constitutes unauthorized or unsolicited advertising, junk, or bulk
+          email.
+        </li>
+      </ul>
+
+      <h2 className="text-xl font-semibold mb-4">License and Usage</h2>
+      <p className="text-justify mb-4">
+        Klipp grants you a non-transferable, non-exclusive license to use the
+        Service per these Terms. Unauthorized use, such as reverse engineering,
+        modifying content, or using Klipp for illicit purposes, will lead to
+        license termination.
+      </p>
+      <h2 className="text-xl font-semibold mb-4">
+        Intellectual Property Rights
+      </h2>
+      <p className="text-justify mb-4">
+        The Service, including its entire content, features, and functionality
+        (such as text, graphics, logos, and software) is owned by Klipp, its
+        licensors, or other content providers. These materials are protected
+        under copyright, trademark, patent, and other intellectual property or
+        proprietary rights laws. By using the Service, you are granted a
+        limited, revocable, non-exclusive license to access and use Klipp for
+        your personal, non-commercial use only.
+      </p>
+
+      <h2 className="text-xl font-semibold mb-4">
+        Disclaimers and Limitation of Liability
+      </h2>
+      <p className="text-justify mb-4">
+        Klipp provides its Service on an "as is" and "as available" basis
+        without any warranties, either express or implied, including but not
+        limited to implied warranties of merchantability, fitness for a
+        particular purpose, or non-infringement. Klipp does not warrant that the
+        Service will always be uninterrupted, timely, secure, or error-free.
+      </p>
+
+      <p className="text-justify mb-4">
+        Klipp shall not be liable for any direct, indirect, incidental, special,
+        consequential, or punitive damages arising out of or related to your use
+        of the Service. This limitation of liability applies to all claims,
+        whether based on warranty, contract, tort, or any other legal theory,
+        and whether or not Klipp is advised of the possibility of such damages.
+      </p>
+
+      <h2 className="text-xl font-semibold mb-4">Contact Information</h2>
+      <p className="text-justify mb-4">
+        If you have any questions about these Terms, please contact us using the
+        information below:
+      </p>
+
+      <p className="text-justify mb-4">
+        Email:{" "}
+        <a
+          href="mailto:klipp.bio@gmail.com"
+          className="text-blue-600 hover:text-blue-800"
+        >
+          klipp.bio@gmail.com
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default TermsOfService;
